ca5205f7a9b5b49e73202c8bcd216bc454d9129c
Kwesi Berko <kwesi_berko@Kwesis-MacBook-Pro.local>
2019-06-15 00:28:27 -0400

    Adding Nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+
+
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+       <Link to="/"><img src={require('../images/logo.svg')} width="30"></img></Link>
+        <link to="/home">Home</link>
+        <link to="/pricing">Pricing</link>
+        <link to="/tourism">Tourism</link>
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react"
+import Link from "gatsby-link"
+
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Heshima">
+      <div className="HeshimaGroup">
+        <h1>Heshimatours</h1>
+        <p>Car Rental & Tours</p>
+        <Link to="/page-2">Learn more</Link>
+      </div>
+    </div>
   </div>
 )
 
